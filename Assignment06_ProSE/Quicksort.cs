dac797837adcf8ea2ccc865314e354eebea0bc7c
Nayun Kim <ny.kim@tum.de>
2023-01-31 10:42:41 +0100

    initiate the project with the base codes
    adding the base code we have to do

---
+++ Assignment06_ProSE/Quicksort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Assignment06_ProSE
+{
+	public class Quicksort
+	{
+		// 1. make the quicksort algorithm which takes intger List as a parameter
+		public static void DoSort(List<int> nums)
+		{
+			// * Using LINQ
+		}
+
+		// 2. Generallize the quicksort with a generic list
+		public static void DoSort<T>(List<T> values)
+		{
+
+		}
+
+		// 2-2. Generallize the with a generic comparision delegate
+		public static void DoSort<T>(Comparison<T> comparision)
+		{
+
+		}
+
+		// 3. Print out the elements of the list
+		public static void PrintOut<T>(List<T> values)
+		{
+
+		}
+	}
+}
+
