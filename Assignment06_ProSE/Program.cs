9b9bede5c2040ff3b28ad15bde62e4b4cd31cb54
Nayun Kim <ny.kim@tum.de>
2023-01-31 10:44:36 +0100

    Update Program.cs

--- Assignment06_ProSE/Program.cs
+++ Assignment06_ProSE/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Assignment06_ProSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4. Call the methods we made
+        }
+    }
+}
 
