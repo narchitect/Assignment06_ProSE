e5e66e2ce4af28625487406031b0954cdc4d6fc1
Nayun Kim <ny.kim@tum.de>
2023-02-06 23:32:54 +0100

    QuickSorts using Linq and Generic Comparision

--- Assignment06_ProSE/Program.cs
+++ Assignment06_ProSE/Program.cs
@@ -6,10 +6,13 @@ namespace Assignment06_ProSE
     {
         static void Main(string[] args)
         {
-            int[] test = { 1, 6, 2, 4, 3, 7, 9, 10, 11, 12, 13 };
-            test = Quicksort.SortArray(test,0,test.Length-1);
+            //List<int> test = new List<int> { 1, 6, 2, 4, 3, 7, 9, 10, 11, 12, 13 };
+            //test = Quicksort.SortArray(test);
 
-            foreach(int i in test)
+            List<double> test2 = new List<double> { 1.2, 3.2, 5.2, 1.5, 8.4 };
+            test2= Quicksort.SortArray(test2);
+
+            foreach(int i in test2)
             {
                 Console.WriteLine(i);   
             }
--- Assignment06_ProSE/Quicksort.cs
+++ Assignment06_ProSE/Quicksort.cs
@@ -1,70 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
 
 namespace Assignment06_ProSE
 {
 	public class Quicksort
 	{
-		// 1. make the quicksort algorithm which takes intger List as a parameter
+		private static int CompareToPivot<T>(T num, T pivot) 
+        {
+			if (Comparer<T>.Default.Compare(num, pivot) > 0) 
+				return 1;
+			else if (Comparer<T>.Default.Compare(num, pivot) == 0)
+				return 0;
+            else
+				return -1;
+        }
 
-		public static int[] SortArray(int[] array, int start_index, int end_index)
+        public static List<T> SortArray<T>(List<T> array)
 		{
-			int pivot, temp;
-			int low, high;
+			List<T> result = array;
+			Comparison<T> Comparer = new Comparison<T>(CompareToPivot);
+			result.Sort(Comparer);
 
-			low = start_index;
-			high = end_index;
-			pivot = array[start_index];
+			return result;
+		}
+		public static List<int> QSLinq(List<int> numbers)
+		{
+			List<int> result = new List<int>();
 
-			//before indices cross each other
-			while (low <= high)
-			{
-				//go to right direction
-				while (array[low] < pivot)
-				{
-					low++;
-				}
-				//go to left dirction
-				while (array[high] > pivot)
-				{
-					high--;
-				}
+			if (numbers.Count <= 1)
+				return numbers;
 
-				// if array[index] out of pivot
-				// and still indices not corss, Swap both
-				if (low <= high)
-				{
-					temp = array[low];
-					array[low] = array[high];
-					array[high] = temp;
-					low++;
-					high--;
-				}
-			}
+			int pivot = numbers[0];
 
-			if (start_index < high)
-				SortArray(array, start_index, high);
-			if (low < end_index)
-				SortArray(array, low, end_index);
-			return array;
+			List<int> less = (from num in numbers where num < pivot select num).ToList();
+			List<int> same = (from num in numbers where num == pivot select num).ToList();
+			List<int> greater = (from num in numbers where num > pivot select num).ToList();
+
+			result = QSLinq(less).Concat(QSLinq(same)).Concat(QSLinq(greater)).ToList();
+
+			return result;
 		}
 
-	
-
-		
-
-        // 2. Generallize the quicksort with a generic list
-        public static void DoSort<T>(List<T> values)
-		{
-
-		}
-
-		// 2-2. Generallize the with a generic comparision delegate
-		public static void DoSort<T>(Comparison<T> comparision)
-		{
-
-		}
 
 		// 3. Print out the elements of the list
 		public static void PrintOut<T>(List<T> values)
